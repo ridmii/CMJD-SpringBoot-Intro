8b22854afa27c0d96433ce6d7e8e58795666a608
Ridmi Vancuylenburg <heyridmi@gmail.com>
2024-10-07 20:23:04 +0530

    security

--- src/main/java/com/ijse/hellospring/repository/UserRepository.java
+++ src/main/java/com/ijse/hellospring/repository/UserRepository.java
@@ -2,9 +2,14 @@ package com.ijse.hellospring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.ijse.hellospring.entity.User;
+import java.util.Optional;
+
+//same as DAO layer in Layered Architecture
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    
-} 
+    //Advanced custom queries
+    Optional<User> findByUsername(String username);
+}
